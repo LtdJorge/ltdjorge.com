932598bfa8660759f76905c5a8df1e49bec053ea
Jorge López <jorgekq@hotmail.es>
2024-09-19 14:23:05 +0200

    Adds i18n middleware

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap'
+// noinspection ES6PreferShortImport
+import {localeList} from "./src/lib/locales.ts";
 
 export default defineConfig({
     site: 'https://ltdjorge.com',
-    integrations: [tailwind(), sitemap()]
+    integrations: [tailwind(), sitemap()],
+    i18n: {
+        defaultLocale: 'en',
+        locales: localeList.map(
+            (locale) => { return locale.locale }
+        ),
+        routing: {
+            prefixDefaultLocale: false,
+        }
+    }
 })
---
+++ src/lib/locales.ts
@@ -0,0 +1,4 @@
+export const localeList = [
+    {locale: 'en', label: 'English', flag: '🇺🇸'},
+    {locale: 'es', label: 'Español', flag: '🇪🇸'}
+];
