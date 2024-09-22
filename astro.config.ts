1972eaef33f96efe0b88b2c96b9b982e62d44ea7
Jorge López <jorgekq@hotmail.es>
2024-09-22 23:34:54 +0200

    Adds compression and inlining plugins configuration

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,28 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap'
+import playformInline from '@playform/inline';
+import playformCompress from '@playform/compress';
+
 // noinspection ES6PreferShortImport
 import {localeList} from "./src/lib/locales.ts";
 
 export default defineConfig({
     site: 'https://ltdjorge.com',
-    integrations: [tailwind(), sitemap()],
+    integrations: [tailwind(), sitemap(), playformCompress({
+        CSS: {
+            csso: {
+                comments: false
+            },
+            lightningcss: {
+                minify: true,
+                unusedSymbols: true
+            }
+        },
+        HTML: true
+    }), playformInline({
+        Path: './src'
+    })],
     i18n: {
         defaultLocale: 'en',
         locales: localeList.map(
