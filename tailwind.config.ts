4d0444a28fff4590109272bffc45b906df5cd0e3
Jorge López <jorgekq@hotmail.es>
2024-09-22 23:35:18 +0200

    Configures Tailwind for print: directives

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
 import type { Config } from 'tailwindcss'
-import typography from '@tailwindcss/typography'
 import typographyPlugin from "@tailwindcss/typography";
 
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
-        extend: {},
-        typography: {}
+        extend: {
+            screens: {
+                print: { raw: 'print' }
+            }
+        },
     },
     plugins: [typographyPlugin],
 } satisfies Config;
