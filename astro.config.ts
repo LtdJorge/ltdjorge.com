a71c5c975c6fc94971708b2db14d18ee3032ebf1
Jorge López <jorgekq@hotmail.es>
2025-06-16 00:48:42 +0200

    Move to Bun and upgrade dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap'
 import playformCompress from '@playform/compress';
 
@@ -8,32 +7,39 @@ import {localeList} from "./src/lib/locales.ts";
 
 import mdx from '@astrojs/mdx';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-    site: 'https://ltdjorge.com',
-    integrations: [tailwind(), sitemap(), playformCompress({
-        CSS: {
-            csso: {
-                comments: false
-            },
-            lightningcss: {
-                minify: true,
-                unusedSymbols: true
-            }
-        },
-        HTML: true
-    })],
-    build: {
-        inlineStylesheets: "always",
-        format: "file"
-    },
-    i18n: {
-        defaultLocale: 'en',
-        locales: localeList.map(
-            (locale) => { return locale.locale }
-        ),
-        routing: {
-            prefixDefaultLocale: false,
-        }
-    }
+  site: 'https://ltdjorge.com',
+
+  integrations: [sitemap(), playformCompress({
+      CSS: {
+          csso: {
+              comments: false
+          },
+          lightningcss: {
+              minify: true,
+          }
+      },
+      HTML: true
   }), mdx()],
+
+  build: {
+      inlineStylesheets: "always",
+      format: "file"
+  },
+
+  i18n: {
+      defaultLocale: 'en',
+      locales: localeList.map(
+          (locale) => { return locale.locale }
+      ),
+      routing: {
+          prefixDefaultLocale: false,
+      }
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
